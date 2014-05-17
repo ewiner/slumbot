3645d838afddbc6fb13ac29f41a091a8d8e488c0
Eric Winer <enwiner@gmail.com>
2014-05-17 08:52:48 -0400

    some simple request and error logging

--- app/controllers/BuildingController.scala
+++ app/controllers/BuildingController.scala
@@ -5,13 +5,18 @@ import scala.concurrent.{Future, ExecutionContext}
 import play.api.libs.json._
 import models._
 import scala.Some
+import play.api.Logger
+import scala.util.Failure
 
 object BuildingController extends Controller {
 
-  def infoPage(placeRef: String) = Action.async {
+  def infoPage(placeRef: String) = Action.async { request =>
     implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
-    GooglePlace.get(placeRef).map(place => Ok(views.html.info(place, SubInfoFormatter.all)))
+    GooglePlace.get(placeRef).map{ place =>
+      Logger.info(s"Reqeust from ${request.remoteAddress} of ${place.name}")
+      Ok(views.html.info(place, SubInfoFormatter.all))
+    }
   }
 
   def subInfo(placeRef: String, sourceSlug: String) = Action.async {
@@ -28,10 +33,15 @@ object BuildingController extends Controller {
           Ok(Json.toJson(result)) // TODO: error checking
         }
     }
+
   }
 
   private def processSubInfo[I](place: GooglePlace, subInfo: SubInfoFormatter[I])(implicit context: ExecutionContext) = {
-    subInfo.dataSource.get(place).map(data => subInfo.format(place, data))
+    subInfo.dataSource.get(place)
+      .map(data => subInfo.format(place, data))
+      .andThen{
+        case Failure(exc) => Logger.error(s"Error retrieving ${subInfo.slug} for ${place.name}", exc)
+      }
   }
 }
 
